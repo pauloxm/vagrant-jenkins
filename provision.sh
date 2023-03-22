638edb14dd93aa246288bdd2c30ed4a455bd36e6
Paulo Xavier <pauloxmachado@gmail.com>
2023-03-22 14:22:08 -0300

    Sintax adjust

--- provision.sh
+++ provision.sh
@@ -2,9 +2,10 @@
 
 ## Instalação do Jenkins
 yum install -y epel-release
+yum install -y wget
 wget --no-check-certificate -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
-yum install -y wget java-11-openjdk java-11-openjdk-devel
+yum install -y java-11-openjdk java-11-openjdk-devel
 yum install -y jenkins
 systemctl daemon-reload
 systemctl start jenkins
@@ -12,10 +13,11 @@ systemctl enable jenkins
 
 ## Instalação do Docker / Docker Compose
 yum install -y yum-utils
-yum-config-manager --add-repo https://download.docker.com/linux/centos.docker-ce.repo
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 yum install -y docker-ce docker-ce-cli containerd.io
 systemctl start docker
 systemctl enable docker
-curl -L "https://github.com/docker/compose/releases/dwnload/v2.17.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+curl -L "https://github.com/docker/compose/releases/download/v2.17.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+systemctl restart docker
